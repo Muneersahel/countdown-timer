96d590d15c8541a5f89fa7dfe29039f1779c27bc
Muneersahel <saidmunir73@gmail.com>
2021-01-03 02:22:32 +0300

    change background

--- dist/script.js
+++ dist/script.js
@@ -9,15 +9,15 @@ function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalseconds = (newYearsDate - currentDate) / 1000;
-    const days = Math.floor(totalseconds / 3600 / 24);
-    const hours = Math.floor(totalseconds / 3600) % 24;
-    const mins = Math.floor(totalseconds / 60) % 60;
-    const seconds = Math.floor(totalseconds % 60);
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
 
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
+    minsEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
 }
 
@@ -25,7 +25,7 @@ function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
-// initiall call
+// initial call
 countdown();
 
 setInterval(countdown, 1000);
